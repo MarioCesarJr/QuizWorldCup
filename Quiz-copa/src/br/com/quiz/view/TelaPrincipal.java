6ebd8a5a36d1d66f09683d0c558bb20d9871e7e4
MarioCesarJr <jun.souza.d@hotmail.com>
2014-05-05 11:11:42 -0300

    Alteração no menu principal

--- Quiz-copa/src/br/com/quiz/view/TelaPrincipal.java
+++ Quiz-copa/src/br/com/quiz/view/TelaPrincipal.java
@@ -1,22 +1,18 @@
 package br.com.quiz.view;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Font;
 
 public class TelaPrincipal extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
@@ -39,31 +35,22 @@ public class TelaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPrincipal() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 822, 465);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 434, 262);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
 		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\junior\\workspace\\Quiz\\img\\footballQuiz.jpeg"));
-		lblNewLabel.setBounds(0, 0, 434, 262);
-		panel.add(lblNewLabel);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\junior\\git\\Quiz-worldcup\\Quiz-copa\\img\\Arena.png"));
+		lblNewLabel.setBounds(0, 11, 744, 405);
+		contentPane.add(lblNewLabel);
 		
-		JButton btnPlay = new JButton("PLAY");
-		btnPlay.setBounds(316, 228, 89, 23);
-		panel.add(btnPlay);
-		btnPlay.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				new TelaPerguntas().setVisible(true);
-				setVisible(false);
-			}
-		});
+		JButton btnIniciar = new JButton("Iniciar");
+		btnIniciar.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnIniciar.setBounds(362, 249, 89, 23);
+		contentPane.add(btnIniciar);
 	}
 }
